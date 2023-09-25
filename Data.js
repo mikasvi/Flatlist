8f806e67a702443fae02885cc19dbe56796549aa
Mika Sarajärvi <mika.sarajarvi@gmail.com>
2023-09-25 18:46:17 +0300

    Commit project

---
+++ Data.js
@@ -0,0 +1,17 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Doe',
+        firstname: 'John',
+    },
+    {
+        id: '2',
+        lastname: 'Smith',
+        firstname: 'Jane',
+    },
+    {
+        id: '3',
+        lastname: 'Unknown',
+        firstname: 'Person',
+    }
+]
